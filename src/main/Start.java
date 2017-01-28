eafc0829f8e9a007710b98ff62ab4f3f6554ed4c
SSVsSunSt <Sergak10@mail.ru>
2017-01-28 20:42:22 +0300

    14

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,14 +1,14 @@
 package main;
 
-import gui.GamePanel;
 import gui.MainGameFrame;
+import gui.MenuPanel;
 
 public class Start {
 	
 	public static void main(String[] args) {
 		
 		MainGameFrame frame = new MainGameFrame();
-		frame.add(new GamePanel(frame, null));
+		frame.add(new MenuPanel(frame));
 		
 	}
 	
